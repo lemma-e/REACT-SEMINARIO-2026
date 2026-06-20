7966d4174bbfe604ee9d83106d82493f604a7c0e
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-06-20 14:29:35 -0300

    initial commit
    Add two example apps: examples/inicial/inicial and examples/proyecto-ejemplo-clase-1/18-outputting-list-data. Each example includes a Vite React setup (index.html, vite.config.js, package.json), src files (App.jsx, index.jsx, components, styles, data) and associated assets. These examples provide starter code and demos for React Essentials lessons.

---
+++ examples/inicial/inicial/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+
+function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+        </section>
+        <p>{selectedTopic}</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
